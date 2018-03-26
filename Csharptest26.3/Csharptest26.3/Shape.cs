060de954f2d2ad2022a8504b212747a092c1f354
Unknown <nikolas.dkan@gmail.com>
2018-03-26 20:49:36 +0300

    //creation of class Shape

---
+++ Csharptest26.3/Csharptest26.3/Shape.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharptest26._3
+{
+    public abstract class Shape
+    {
+
+    }
+}
