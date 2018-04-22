e34de5bc67cb86f839bca2c8c3fc87dd25bb4665
Nikolas D. Kanellopoulos <nikolas.dkan@gmail.com>
2018-04-22 12:41:03 +0300

    //updated the enums

---
+++ MiniEmailApp/MiniEmailApp/Enums/GodLoginMenuOption.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniEmailApp.Enums
+{
+    class GodLoginMenuOption
+    {
+        enum SuperAdmiLoginMenuOption
+        {
+            ReadMessageBox = 1,
+            SentMessage,
+            EnterChatroom,
+            ChangePersonalInfo,
+            SelectUsertoViewhisChatHistory,
+            DeleteUserMailbox,
+            ResetUserPassword,
+            DeleteUser,
+            DeletAllDatabase,
+            GrantSuperAdminPriveleges,
+            DepositmillinDollars,
+            Logout
+        }
+    }
+}
---
+++ MiniEmailApp/MiniEmailApp/Enums/SuperAdmiLoginMenuOption.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniEmailApp.Enums
+{
+    enum SuperAdmiLoginMenuOption
+    {
+        ReadMessageBox = 1,
+        SentMessage,
+        EnterChatroom,
+        ChangePersonalInfo,
+        SelectUsertoViewhisChatHistory,
+        DeleteUserMailbox,
+        ResetUserPassword,
+        DeleteUser,
+        Logout
+    }
+}
