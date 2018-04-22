7082243bf5dcaedc0e3d1206a60ed0dc1275f6a4
Nikolas D. Kanellopoulos <nikolas.dkan@gmail.com>
2018-04-22 12:28:47 +0300

    /Create a folder and added all the User,Admin,SuperAdmin etc. Menus Inside

---
+++ MiniEmailApp/MiniEmailApp/Enums/AdminLoginMenuOption.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniEmailApp.Enums
+{
+    enum AdminLoginMenuOption
+    {
+        ReadMessageBox=1,
+        SentMessage,
+        EnterChatroom,
+        ChangePersonalInfo,
+        SelectUsertoViewhisChatHistory,
+        DeleteUserMailbox,
+        ResetUserPassword,
+        Logout
+    }
+}
